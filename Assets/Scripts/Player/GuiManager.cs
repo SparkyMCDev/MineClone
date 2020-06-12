61e29034c6f93d0c45212baa8bfb31861546b617
Sparky200 <48421896+Sparky200@users.noreply.github.com>
2020-06-12 17:55:38 -0500

    Gui System and Crosshair

---
+++ Assets/Scripts/Player/GuiManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuiManager : MonoBehaviour
+{
+    public List<GameObject> guis;
+    public static GuiManager Instance { get; internal set; }
+
+    private void Awake()
+    {
+        guis = new List<GameObject>();
+
+        Instance = this;
+
+        GameObject gui_crosshair = new GameObject("crosshair");
+        gui_crosshair.transform.parent = transform;
+        gui_crosshair.AddComponent<Image>().sprite = Resources.Load<Sprite>("gui/img/crosshair");
+
+        gui_crosshair.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        gui_crosshair.GetComponent<RectTransform>().sizeDelta = new Vector2(40, 40);
+    }
+
+    private void Update()
+    {
+        foreach(Transform g in transform.GetComponentsInChildren<Transform>())
+        {
+            if (!guis.Contains(g.gameObject)) guis.Add(g.gameObject);
+        }
+        
+
+    }
+}
