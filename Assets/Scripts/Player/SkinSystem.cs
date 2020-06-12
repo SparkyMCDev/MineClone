05a47f9e2ac9acbb815859cf1d75d6e6db287cc9
_Cookie_ <weidlichnathan@gmail.com>
2020-06-12 16:24:14 -0700

    Added simple player skin system

---
+++ Assets/Scripts/Player/SkinSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkinSystem : MonoBehaviour
+{
+    private Texture Skin;
+    private string SkinFilePath = "PlayerSkin";
+    private string DefaultPlayerSkinFilePath = "DefaultPlayerSkin";
+    Renderer m_Renderer;
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        m_Renderer.material.SetTexture("_MainTex", getSkin());
+    }
+
+    private Texture getSkin()
+    {
+        Skin = Resources.Load<Texture>("textures/player/" + SkinFilePath);
+        if (Skin == null)
+        {
+            return Resources.Load<Texture>("textures/player/" + DefaultPlayerSkinFilePath);
+        }
+        else { return Skin; }
+    }
+}
